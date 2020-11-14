b11a5b290fec9a775d9c9392f11d1d59842bcf53
Suryan Varun <varunsuryan9@gmail.com>
2020-11-13 21:50:59 -0500

    Correct NN architecture Pytorch-mushroom implementation done

--- pytorch-Mushroom.py
+++ pytorch-Mushroom.py
@@ -4,8 +4,9 @@ from torch import nn
 import torch.nn.functional as F
 import torch.optim as optim
 from tqdm import tqdm
+import warnings
 df = pd.read_csv('C:/Users/Varun Suryan/Desktop/ML Guarnatess/Project/data/mushrooms.csv')
-
+warnings.filterwarnings("ignore")
 
 K = 50
 
@@ -48,11 +49,11 @@ class Net(nn.Module):
 
 
     def forward(self, x):
-    	x = self.fc1(x)
-    	x = F.relu(x)
-    	x = self.dropout1(x)
-    	x = self.fc2(x)
-    	return x
+      x = self.fc1(x)
+      x = F.relu(x)
+      x = self.dropout1(x)
+      x = self.fc2(x)
+      return x
 
 
 random_data = torch.rand((1, X.shape[1]))
@@ -63,7 +64,7 @@ loss_fn = nn.MSELoss(reduction = 'sum')
 optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
 
 def df_to_tensor(df):
-   	return torch.from_numpy(df.values).float()
+    return torch.from_numpy(df.values).float()
 
 
 for t in range(500):
@@ -75,18 +76,24 @@ for t in range(500):
     optimizer.step()
 
 
-
 # values, indices = model(df_to_tensor(X)).topk(K, 0)
 
-for _ in range(10):
+for _ in range(100):
 
-  values, indices = model(df_to_tensor(X)).topk(K, 0)
-  eaten = pd.Series(indices[:, 0])
-  
-  eaten_X = pd.concat([eaten_X, X.loc[eaten, :]])
-  eaten_y = pd.concat([eaten_y, y[eaten]])
-  
-  X, y = X.drop(eaten), y.drop(eaten)
+  for t in range(1000):
+    # Forward pass: compute predicted y by passing x to the model.
+    y_pred = model(df_to_tensor(eaten_X))
+    loss = loss_fn(y_pred, df_to_tensor(eaten_y))
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+  values = model(df_to_tensor(X))
+  eaten = pd.Series(values[:, 0].detach().numpy(), index = X.index).sort_values(ascending = False).head(K).index
+  eaten_X = pd.concat([eaten_X, X.loc[eaten]])
+  eaten_y = pd.concat([eaten_y, y.loc[eaten]])
+  print(eaten_y.sum())
+  X, y = X.drop(X.loc[eaten, :].index), y.drop(X.loc[eaten, :].index)
 
 
 # print(df_to_tensor(y)[indices])
@@ -97,9 +104,9 @@ for _ in range(10):
 
 
 class mushrooms:
-	def __init__(self, X, y):
-		self.X = X
-		self.y = y
+  def __init__(self, X, y):
+    self.X = X
+    self.y = y
 
-	def eaten(self, indices):
-		self.indices = indices
+  def eaten(self, indices):
+    self.indices = indices
