2acf36588cf6ab465075d0101d88588681397910
Suryan Varun <varunsuryan9@gmail.com>
2020-11-15 00:23:32 -0500

    Pytorch mushroom finding optimal solution

--- pytorch-Mushroom.py
+++ pytorch-Mushroom.py
@@ -8,11 +8,12 @@ import warnings
 df = pd.read_csv('C:/Users/Varun Suryan/Desktop/ML Guarnatess/Project/data/mushrooms.csv')
 warnings.filterwarnings("ignore")
 
-K = 50
+K = 100
 
 # lablels of mushrooms
 y = df['class'].astype("category").cat.codes
 
+# print(y.value_counts())
 # charactersitcis
 X = pd.get_dummies(df.drop('class', 1), drop_first = True)
 
@@ -40,29 +41,37 @@ class Net(nn.Module):
       super(Net, self).__init__()
 
       # First fully connected layer
-      self.fc1 = nn.Linear(self.input_dim, 128)
-      
-      self.dropout1 = nn.Dropout2d(0.25)
-
+      self.fc1 = nn.Linear(self.input_dim, 16)
+      self.dropout1 = nn.Dropout2d(0.5)
+      self.fc2 = nn.Linear(16, 16)
+      self.dropout2 = nn.Dropout2d(0.5)
       # Second fully connected layer that outputs our label
-      self.fc2 = nn.Linear(128, 1)
-
+      self.fc3 = nn.Linear(16, 16)
+      self.fc4 = nn.Linear(16, 1)
+      self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
       x = self.fc1(x)
       x = F.relu(x)
       x = self.dropout1(x)
-      x = self.fc2(x)
+      # x = self.fc2(x)
+      # x = F.relu(x)
+      # x = self.dropout2(x)
+      x = self.fc3(x)
+      x = F.relu(x)
+      x = self.fc4(x)
+      x = self.sigmoid(x)
       return x
 
 
 random_data = torch.rand((1, X.shape[1]))
 model = Net(X.shape[1])
 
-learning_rate = 1e-4
-loss_fn = nn.MSELoss(reduction = 'sum')
-optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
-
+learning_rate = 1e-3
+loss_fn = nn.BCELoss()
+# loss_fn = nn.MSELoss()
+# optimizer = optim.SGD(model.parameters(), lr = learning_rate, momentum=0.9)
+optimizer = optim.Adam(model.parameters(), lr=0.0001)
 def df_to_tensor(df):
     return torch.from_numpy(df.values).float()
 
@@ -78,9 +87,9 @@ for t in range(500):
 
 # values, indices = model(df_to_tensor(X)).topk(K, 0)
 
-for _ in range(100):
+for _ in range(80):
 
-  for t in range(1000):
+  for t in range(100):
     # Forward pass: compute predicted y by passing x to the model.
     y_pred = model(df_to_tensor(eaten_X))
     loss = loss_fn(y_pred, df_to_tensor(eaten_y))
