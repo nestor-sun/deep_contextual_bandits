df17df04a13380a55ff89db961a09aaf12356036
Michael Rawson <michrawson@users.noreply.github.com>
2020-11-28 13:33:42 -0500

    change ranking algo

--- ranking/main.py
+++ ranking/main.py
@@ -2,76 +2,21 @@ import numpy as np
 import itertools
 from itertools import permutations
 
-context_list = np.zeros(9) # change to list of users in dataset
-number_of_categories = 9 # change to number of categories in dataset
 
-print(context_list)
+# Top k-Ranking 
 
-def expected_reward(arm,context,reward_history):
-    if len(reward_history)==0:
-        return 0
-    else:
-        r=0
-        for ranking_reward in reward_history:
-            r = r + ranking_reward[1]
-        return r/len(reward_history)
+def get_ranking(k, context, history):
 
-def get_ranking(tuple_valuation):
-    ranking = []
-    for t in sorted(tuple_valuation, key=tuple_valuation.__getitem__):
-        if isinstance(t, int):
-            ranking.append(t)
-    return ranking
+# context is user and/or search string
 
-def submit_action(ranking):
-    return np.random.random(1)
+choosen_arms = np.zeros(arm_len)
+ranking = np.zeros(k)
 
-def average_ranking(ranking,t):
-    if isinstance(t, int):
-        return ranking.index(t)+1
-    else:
-        tuple_ranks = []
-        for o in t:
-            tuple_ranks.append(ranking.index(o)+1)
-        return np.mean(tuple_ranks)
+for i = range(k):
+    ranking(i) = MAB(concat(context,choosen_arms), history) # chooses an arm from the context 
 
-# tuple_list = list(permutations([1,2]))
-tuple_list = []
-for i in range(1,number_of_categories+1):
-    tuple_list.append(i)
-for i in range(1,number_of_categories+1):
-    for j in range(i+1,number_of_categories+1):
-        tuple_list.append((i,j))
+    choosen_arms(ranking(i))=1
 
-print(tuple_list)
 
-tuple_valuation = {}
-reward_history = {}
-for t in tuple_list:
-    tuple_valuation[t] = 0
-    reward_history[t] = []
-
-print(tuple_valuation)
-print(reward_history)
-
-for context in context_list:
-    for t in tuple_list:
-        tuple_valuation[t] = expected_reward(t,context,reward_history[t])
-    print('tuple_valuation')
-    print(tuple_valuation)
-
-    ranking = get_ranking(tuple_valuation)
-
-    print('ranking')
-    print(ranking)
-    reward = submit_action(ranking)
-    print('reward')
-    print(reward)
-    for t in tuple_list:
-        # print(t)
-        reward_history[t].append([average_ranking(ranking,t),reward])
-        # print(average_ranking(ranking,t))
-    # break
-    # print('reward_history')
-    # print(reward_history)
+return ranking
 
