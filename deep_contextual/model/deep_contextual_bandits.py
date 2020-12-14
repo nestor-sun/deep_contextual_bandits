901a97deb5dd9e159c541b1bc0a53d61d2f1f4b3
nestor-sun <47902113+nestor-sun@users.noreply.github.com>
2020-12-14 04:43:53 -0500

    Add files via upload

--- deep_contextual/model/deep_contextual_bandits.py
+++ deep_contextual/model/deep_contextual_bandits.py
@@ -5,28 +5,38 @@ import torch.nn.functional as F
 
 
 class DeepContextualBandits(nn.Module):
-    def __init__(self, user_feat_num):
+    def __init__(self, feat_num):
         super(DeepContextualBandits, self).__init__()
-        self.user_feat_num = user_feat_num
+        self.feat_num = feat_num
 
-        self.resnet = models.resnet34(pretrained=True)
-        for para in self.resnet.parameters():
-            para.requires_grad = False
+        # self.resnet = models.resnet34(pretrained=True)
+        # for para in self.resnet.parameters():
+        #     para.requires_grad = False
+        #
+        # self.resnet.fc = nn.Sequential(
+        #     nn.Linear(512, 256),
+        #     nn.ReLU(),
+        #     nn.Dropout(0.2)
+        # )
 
-        self.resnet.fc = nn.Sequential(
-            nn.Linear(512, 256),
+        self.fc = nn.Sequential(
+            nn.Linear(feat_num, 58),
             nn.ReLU(),
-            nn.Dropout(0.2)
+            nn.Linear(58, 56),
+            nn.ReLU(),
+            nn.Linear(56, 56),
+            nn.ReLU(),
+            nn.Linear(56, 28),
+            nn.ReLU(),
+            nn.Linear(28, 28),
+            nn.ReLU(),
+            nn.Linear(28, 1),
         )
 
-        self.fc1 = nn.Sequential(
-            nn.Linear(256 + user_feat_num, 128),
-            nn.Linear(128, 1)
-        )
-
-    def forward(self, image, user):
-        out = self.resnet(image).squeeze(0)
-        out = torch.cat((out, user), dim=-1)
-        out = self.fc1(out)
+    def forward(self, feat):
+        out = self.fc(feat)
         return out
+
+
+
 
--- deep_contextual/model/train.py
+++ deep_contextual/model/train.py
@@ -1,71 +1,89 @@
 import sys
-sys.path.append('D:/study/gumd/UMD_course/machine learning guarantees and analyses/deep_contextual_bandits/code/model')
+sys.path.append('deep_contextual/model')
 
 from deep_contextual_bandits import DeepContextualBandits
 from dataloader import Data
-from torch.utils.data import DataLoader
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import torchvision.transforms as transforms
-import torchvision.models as models
 from torch.autograd import Variable
+import json
+import pandas as pd
 
 
-learning_rate = 0.001
-model = DeepContextualBandits(150)
+learning_rate = 0.0001
+model = DeepContextualBandits(60)
 # Loss and optimizer
 criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 
-img_top_dir = 'D:/study/gumd/UMD_course/machine learning guarantees and analyses/deep_contextual_bandits/data/ads_16/ADS16_Benchmark_part1/Ads/'
 training_data_file = 'D:/study/gumd/UMD_course/machine learning guarantees and analyses/deep_contextual_bandits/data/ads_16/ADS16_Benchmark_part1/training.json'
-transform = transforms.Compose([transforms.Resize(256), transforms.CenterCrop(224), transforms.ToTensor(), \
-                                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
-train_data = Data(img_top_dir, training_data_file, transform)
-loader = DataLoader(train_data, batch_size=12, shuffle=True, num_workers=2)
+train_data = Data(training_data_file)
+# loader = DataLoader(train_data, batch_size=12, shuffle=True, num_workers=2)
 
-# model = models.resnet50(pretrained=True)
-# img = train_data[0]['img']
-# user = train_data[0]['user_feat']
-#
-# model(img.unsqueeze(0))
-
+# print(train_data[0][0], train_data[0][1])
 
 if torch.cuda.is_available():
-    model.cuda()
+    model = model.cuda()
     criterion = criterion.cuda()
 
 
 def train():
     train_losses = []
     model.train()
-    for i, data in enumerate(loader):
-        img = Variable(train_data[i]['img'])
-        user_feat = Variable(train_data[i]['user_feat'])
-        y = Variable(train_data[i]['y']).double()
+    for i, data_row in enumerate(train_data):
+        feat = data_row[0]
+        # print(len(feat))
+        y = data_row[1]
+
+        feat = Variable(feat)
+        y = Variable(y).double()
 
         # converting the data into GPU format
         if torch.cuda.is_available():
-            img = img.cuda()
-            user_feat = user_feat.cuda()
+            feat = feat.cuda()
             y = y.cuda()
 
-        output_train = model(img.unsqueeze(0), user_feat)
+        output_train = model(feat)
         output_train = output_train.double()
         # clearing the Gradients of the model parameters
         optimizer.zero_grad()
         loss_train = criterion(output_train, y)
         train_losses.append(loss_train)
-        train_losses.append(loss_train)
+
         # computing the updated weights of all the model parameters
         loss_train.backward()
         optimizer.step()
-    return sum(train_losses)
+    return float(sum(train_losses))
 
 
-for epoch in range(200):
-    loss = train()
+loss_list = []
+for epoch in range(250):
+    loss = train()/len(train_data)
+    loss_list.append(loss)
     print('Epoch', epoch, 'Loss', loss)
+
+output_path = 'D:/study/gumd/UMD_course/machine learning guarantees and analyses/deep_contextual_bandits/deep_contextual/model/'
+json.dump([float(i) for i in loss_list], open(output_path + 'loss_list.json', 'w'))
+torch.save(model, output_path + 'model.dat')
+
+testing_data_file = 'D:/study/gumd/UMD_course/machine learning guarantees and analyses/deep_contextual_bandits/data/ads_16/ADS16_Benchmark_part2/testing.json'
+testing_data = Data(testing_data_file)
+
+data = []
+for i, data_row in enumerate(testing_data):
+    feat = data_row[0]
+    y = data_row[1]
+
+    if torch.cuda.is_available():
+        feat = feat.cuda()
+        y = y.cuda()
+
+    output = model(feat)
+    # print(output)
+    data.append({'prediction': float(output), 'target': float(y)})
+
+df = pd.DataFrame(data)
+print(df.corr())
+
 
